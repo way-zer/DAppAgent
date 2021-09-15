65927c81240cd0aa2333435fc14eca5ac75a462f
way-zer <himc.wicp@gmail.com>
2021-09-15 14:43:08 +0800

    增加本地App开发用网关代理

--- src/apis/gateway.ts
+++ src/apis/gateway.ts
@@ -3,6 +3,9 @@ import {getType} from 'mime/lite'
 import readable from 'it-to-stream'
 import {controller, DarukContext, get, priority} from 'daruk'
 import {useApp} from './hooks/useApp'
+import proxy from 'koa-better-http-proxy'
+import {boomify} from '@hapi/boom'
+import {constants} from 'http2'
 
 @controller()
 @priority(1)
@@ -10,6 +13,17 @@ export class _Gateway {
     @get('/(.*)')
     async get(ctx: DarukContext) {
         const app = await useApp(ctx)
+        const devPort = ctx.host.split(':')[1]
+        if (devPort) {
+            //forward to frontend dev server
+            try {
+                return await proxy('localhost:' + devPort, {})(ctx, Promise.resolve)
+            } catch (e) {
+                throw boomify(e, {
+                    statusCode: constants.HTTP_STATUS_BAD_GATEWAY,
+                })
+            }
+        }
         let path = ctx.path
         if (path.endsWith('/'))
             path += 'index.html'
