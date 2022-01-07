35c82beb0cdff6e381d975f56d63390503c3c4f5
way-zer <himc.wicp@gmail.com>
2022-01-07 13:53:31 +0800

    (frontend-admin) 使用库react-use-promise重写IpfsInfo AppInfo

--- packages/frontend-admin/src/page/AppInfo.tsx
+++ packages/frontend-admin/src/page/AppInfo.tsx
@@ -1,68 +1,50 @@
-import { list, publish } from "@api/apps";
-import React from "react";
-import { Table, message, Popconfirm } from 'antd';
+import React from 'react';
+import {Divider, message, Popconfirm as PopConfirm, Table} from 'antd';
+import {useService} from 'sdk';
+import usePromise from 'react-use-promise';
 
+const apps = useService('apps');
 
-export default class AppInfo extends React.Component {
-    state = {
-        data: [],
-    }
+export default function AppInfo() {
+  const [data] = usePromise(() => apps.listPrivate().then(data =>
+    Object.entries(data).map(([name, value]) => ({name, ...value})),
+  ), []);
 
-    componentDidMount() {
-        list().then((res) => {
-            this.setState({
-                data: Object.keys(res).map((appname) => {
-                    return{
-                        name: appname, 
-                        cid:  res[appname].cid,
-                        prod: res[appname].prod,
-                    }
-                }
-                )}) 
-        })
-    }
-
-    handlePublish = (name: string) => {
-        publish(name).then((res)=>{
-            if(res?.error==='NOT FOUND')
-                message.error('NOT FOUND');
-            else
-                message.success('发布成功')
-        })
-    }
-
-    render() {
-        const columns= [
-            {
-                title: 'name',
-                dataIndex: 'name',
-                key: 'name'
-            },
-            {
-                title: 'cid',
-                dataIndex: 'cid',
-                key: 'cid'
-            },
-            {
-                title: 'prod',
-                dataIndex: 'prod',
-                key: 'prod'
-            },
-            {
-                title: 'action',
-                key: 'action',
-                render: (_, record) => (
-                    <Popconfirm title="Sure to publish?" onConfirm={() => this.handlePublish(record.name)}>
-                    <a>Publish</a>
-                    </Popconfirm>
-                  ),
-            }
-        ]
-
-        return (
-            <div>
-                <Table rowKey={record => record.name} dataSource={this.state.data} columns={columns} />
-            </div>
-        )
-    }
+  return (
+    <div>
+      <Table rowKey={record => record.name} dataSource={data} loading={data === undefined} columns={[
+        {
+          title: 'name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'cid',
+          dataIndex: 'cid',
+          key: 'cid',
+        },
+        {
+          title: 'prod',
+          dataIndex: 'prod',
+          key: 'prod',
+        },
+        {
+          title: 'action',
+          key: 'action',
+          render: (_, record) => (
+            <>
+              <a href={record.url} target="_blank">Open</a>
+              <Divider type={'vertical'}/>
+              <PopConfirm title="Sure to publish?" onConfirm={async () => {
+                await apps.publish(record.name);
+                await message.success('发布成功');
+              }}>
+                <a>Publish</a>
+              </PopConfirm>
+            </>
+          ),
+        },
+      ]}/>
+    </div>
+  );
 }
--- packages/frontend-admin/src/page/IpfsInfo.tsx
+++ packages/frontend-admin/src/page/IpfsInfo.tsx
@@ -1,48 +1,29 @@
-import { status } from "@api/ipfs"
-import { Descriptions, Badge } from "antd"
-import React from "react"
+import {Descriptions} from 'antd';
+import React from 'react';
 import 'antd/dist/antd.css';
-import {Services} from 'sdk';
+import {useService} from 'sdk';
+import usePromise from 'react-use-promise';
 
-export default class IpfsInfo extends React.Component{
-	state = {} as {info?:Awaited<ReturnType<Services["system"]["status"]>>}
-
-	componentDidMount() {
-		status().then(
-			(res) => {
-				console.log(res)
-				this.setState({
-					info: res,
-				})
-			}
-		)
-	}
-
-	genBandwidth = (dataItem: IpfsInfo["state"]["info"]["bandwidth"][0]) => {
-		return(
-			<Descriptions  layout='vertical' bordered column={{xs:1, sm:2,  md:2, lg:4}}>
-				<Descriptions.Item label='rateIn'>{dataItem.rateIn}</Descriptions.Item>
-				<Descriptions.Item label='rateOut'>{dataItem.rateOut}</Descriptions.Item>
-				<Descriptions.Item label='totalIn'>{dataItem.totalIn}</Descriptions.Item>
-				<Descriptions.Item label='totalOut'>{dataItem.totalOut}</Descriptions.Item>
-			</Descriptions>
-		)
-	}
-
-	genPeers = (dataItem: string) => {
-		return(
-			<div>{dataItem}<br/></div>
-		)
-	}
-
-	render() {
-		console.log(this.state.info);
-		return(
-		<Descriptions title="IPFS Info" bordered={true} column={{lg: 2, sm: 1, xs: 1}}>
-		 	<Descriptions.Item label="Running">{this.state.info?.running + ''}</Descriptions.Item>
-		 	<Descriptions.Item label="Bandwidth">{this.state.info?.bandwidth?.map((dataItem) => this.genBandwidth(dataItem))}</Descriptions.Item>
-		 	<Descriptions.Item label='peers'>{this.state.info?.peers?.map((dataItem) => this.genPeers(dataItem))}</Descriptions.Item>
-		</Descriptions>
-		)
-	}
+export default function IpfsInfo() {
+  const [status] = usePromise(() => useService('system').status(), []);
+  return (
+    <Descriptions title="System Info" bordered={true} column={{lg: 2, sm: 1, xs: 1}}>
+      <Descriptions.Item label="IPFS Running">{status?.ipfs + ''}</Descriptions.Item>
+      <Descriptions.Item label="OrbitDB Running">{status?.orbitDB + ''}</Descriptions.Item>
+      <Descriptions.Item label="Bandwidth">
+        {status?.bandwidth?.map((dataItem) => (
+          <Descriptions layout="vertical" bordered column={{xs: 1, sm: 2, md: 2, lg: 4}}>
+            <Descriptions.Item label="rateIn">{dataItem.rateIn}</Descriptions.Item>
+            <Descriptions.Item label="rateOut">{dataItem.rateOut}</Descriptions.Item>
+            <Descriptions.Item label="totalIn">{dataItem.totalIn}</Descriptions.Item>
+            <Descriptions.Item label="totalOut">{dataItem.totalOut}</Descriptions.Item>
+          </Descriptions>
+        ))}
+      </Descriptions.Item>
+      <Descriptions.Item label="peers">
+        {status?.peers?.map((dataItem) => (
+          <div>{JSON.stringify(dataItem)}<br/></div>
+        ))}</Descriptions.Item>
+    </Descriptions>
+  );
 }
