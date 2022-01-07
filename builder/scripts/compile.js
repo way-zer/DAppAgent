0fdebd9d91a9885c8437bedad94ab5c5df5174f5
way-zer <himc.wicp@gmail.com>
2022-01-07 13:22:14 +0800

    (builder) 使用最新版pnpm特性

--- builder/scripts/compile.js
+++ builder/scripts/compile.js
@@ -5,14 +5,5 @@ fs.rmSync('./dist', {recursive: true, force: true});
 fs.cpSync('../packages/main/dist', './dist', {recursive: true});
 fs.cpSync('../packages/preload/dist', './dist/preload', {recursive: true});
 
-//pnpm workspace file
-wFile = '../pnpm-workspace.yaml';
-wFileBak = wFile + '.bak';
-if (fs.existsSync(wFile))
-  fs.renameSync(wFile, wFileBak);
-
-child_process.execSync('pnpm i', {stdio: 'inherit'});
+child_process.execSync('pnpm i --ignore-workspace --config.node-linker=hoisted', {stdio: 'inherit'});
 child_process.execSync('electron-builder build --config electron-builder.config.js --dir', {stdio: 'inherit'});
-
-if (fs.existsSync(wFileBak))
-  fs.renameSync(wFileBak, wFile);
