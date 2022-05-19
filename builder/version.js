26532da5e3438a829dbf84496df1d35aef55cdce
way-zer <himc.wicp@gmail.com>
2022-05-19 20:37:39 +0800

    ci 修改版本号格式

--- builder/version.js
+++ builder/version.js
@@ -1,7 +1,7 @@
 const num = process.env.RUN_NUM || '0-dev';
 const date = new Date();
 
-const year = (date.getFullYear() - 2000);
+const year = date.getFullYear();
 const month = (date.getUTCMonth() + 1);
 const version = `${year}.${month}.${num}`;
 console.log('::set-output name=build-version::' + version);
